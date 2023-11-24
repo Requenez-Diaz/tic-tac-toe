80fe075d1e4f6763492edf7edfec69074e48da5f
Avimilex <95236561+Requenez-Diaz@users.noreply.github.com>
2023-11-23 23:19:05 -0600

    [ADD]: adding page about tictactoe

---
+++ src/app/prueba/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className='flex justify-center items-center h-screen '>
+      <div className='grid grid-cols-3  p-2 border'>
+        <div className='bg-red-500 p-4 text-center border'>1</div>
+        <div className='bg-red-500 p-4 text-center border'>2</div>
+        <div className='bg-red-500 p-4 text-center border'>3</div>
+        <div className='bg-red-500 p-4 text-center border'>4</div>
+        <div className='bg-red-500 p-4 text-center border'>5</div>
+        <div className='bg-red-500 p-4 text-center border'>6</div>
+        <div className='bg-red-500 p-4 text-center border'>7</div>
+        <div className='bg-red-500 p-4 text-center border'>8</div>
+        <div className='bg-red-500 p-4 text-center border'>9</div>
+      </div>
+    </div>
+  );
+}
